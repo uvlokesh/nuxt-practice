fdf081c3ac0eaeac4107fd6dac4bd6f145ed2232
lokesh <uv.lokesh7@gmail.com>
2023-11-29 17:08:31 +0530

    Dynamic params

---
+++ pages/users/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>The User ID is {{ id }}</h1>
+</template>
+
+<script setup>
+const nuxt = useNuxtApp();
+const id = nuxt._route.params.id;
+console.log(nuxt._route.params.id);
+// const route = useRoute();
+// console.log(route.params.id);
+</script>
